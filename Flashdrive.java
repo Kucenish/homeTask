76b11bb76bb3a6be50904d397da145332f2f13bb
Kucenish <luciacia@mail.ru>
2016-10-10 17:04:35 +0300

    Add files via upload

---
+++ Flashdrive.java
@@ -0,0 +1,20 @@
+/**
+ * Created by User on 10.10.2016.
+ */
+public class Flashdrive {
+    public static void main(String[] args) {
+        String manufacturer = "SILICON POWER";
+        String model = "Touch T03";
+        String color = "silver";
+        int wireLength = 2;
+        int price = 149;
+        int memory = 16;
+        System.out.println("Флешка");
+        System.out.println("Производитель " + manufacturer);
+        System.out.println("Модель " + model);
+        System.out.println("Объем памяти " + memory +" Gb");
+        System.out.println("Цвет - " + color);
+        System.out.println("Длина наушников " + wireLength +" метра");
+        System.out.println("Стомость " + price +" грн");
+    }
+}
