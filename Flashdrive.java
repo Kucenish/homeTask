d79c9d0fccd07b3a7c0011670e44f43383850c7e
Kucenish <luciacia@mail.ru>
2016-10-12 12:28:40 +0300

    Update Flashdrive.java

--- Flashdrive.java
+++ Flashdrive.java
@@ -6,7 +6,6 @@ public class Flashdrive {
         String manufacturer = "SILICON POWER";
         String model = "Touch T03";
         String color = "silver";
-        int wireLength = 2;
         int price = 149;
         int memory = 16;
         System.out.println("Флешка");
@@ -14,7 +13,6 @@ public class Flashdrive {
         System.out.println("Модель " + model);
         System.out.println("Объем памяти " + memory +" Gb");
         System.out.println("Цвет - " + color);
-        System.out.println("Длина наушников " + wireLength +" метра");
         System.out.println("Стомость " + price +" грн");
     }
 }
