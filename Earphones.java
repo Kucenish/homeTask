01fa23d65f300278993c5c57ed6c4b457ed50faa
Kucenish <luciacia@mail.ru>
2016-10-12 12:38:09 +0300

    Add files via upload

---
+++ Earphones.java
@@ -0,0 +1,18 @@
+/**
+ * Created by User on 12.10.2016.
+ */
+public class Earphones {
+    public static void main(String[] args) {
+        String manufacturer ="PHILIPS";
+        String model = "SHQ1300LF/00";
+        String color = "серый/оливковый";
+        int wireLenght = 2;
+        int price = 349;
+        System.out.println("Наушники");
+        System.out.println("Изготовитель " + manufacturer);
+        System.out.println("Модель " + model);
+        System.out.println("Цвет " + color);
+        System.out.println("Длина наушников " + wireLenght +" м");
+        System.out.println("Цена " +price +" грн");
+    }
+}
