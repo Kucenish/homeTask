362d21612a24c52a6a160c9286606ca3c882a1e3
Kucenish <luciacia@mail.ru>
2016-10-12 12:21:26 +0300

    Add files via upload

---
+++ Electrosamocat.java
@@ -0,0 +1,19 @@
+/**
+ * Created by User on 12.10.2016.
+ */
+public class Electrosamocat {
+    public static void main(String[] args) {
+        String manufacturer = "Inmotion";
+        String model = "ES05";
+        String type = "детский";
+        String color = "Yellow";
+        int price = 5499;
+        System.out.println("Элестросамокат");
+        System.out.println("Изготовитель " + manufacturer);
+        System.out.println("Модель " + model);
+        System.out.println("Цвет " + color);
+        System.out.println("Цена " + price + " грн");
+    }
+
+
+}
